ce97c318c49ec8100d6f8dbb6805c89647fd1db0
Matin-Rasoulzad <154085709+Matin-Rasoulzad@users.noreply.github.com>
2024-12-08 20:12:55 +0330

    Working prefix & post fix

---
+++ main.py
@@ -0,0 +1,57 @@
+class ExpressionCalculator:
+    precedence = {"(": 1, ")": 1, "^": 2, "*": 3, "/": 3, "+": 4, "-": 4}
+
+    @classmethod
+    def infix2postfix(cls, infix):
+        stack = []
+        postfix = []
+
+        for element in infix:
+
+            if "-" in element and element.isalnum:
+                postfix.append(element)
+
+
+            elif element.isalnum():
+                # alphabet letter (a-z) and numbers (0-9)
+                postfix.append(element)
+
+
+
+            elif element == '(':
+                stack.append(element)
+            elif element == ')':
+                while stack and stack[-1] != '(':
+                    postfix.append(stack.pop())
+                if not stack:
+                    raise ValueError("Mismatched parentheses in expression")
+                stack.pop()  # Remove ( from the stack
+            else:
+                if stack and not cls.precedence.get(stack[-1]) == 1:
+                    while (stack and cls.precedence.get(element, 0) <= cls.precedence.get(stack[-1], 0)):
+                        # Send element as a key, if not found, return 0
+                        postfix.append(stack.pop())
+                stack.append(element)
+
+        # Pop any remaining operators from the stack
+        while stack:
+            if stack[-1] == '(' or stack[-1] == ')':
+                raise ValueError("Mismatched parentheses in expression")
+            postfix.append(stack.pop())
+
+        return ' '.join(postfix)
+
+    @classmethod
+    def infix2prefix(cls, infix):
+        reversed_infix = []
+        for element in reversed(infix):
+            if element == '(':
+                reversed_infix.append(')')
+            elif element == ')':
+                reversed_infix.append('(')
+            else:
+                reversed_infix.append(element)
+
+        reversed_postfix = cls.infix2postfix(reversed_infix)
+        prefix = reversed_postfix.split()[::-1]
+        return ' '.join(prefix)
